93e346d67f2ccd3f11d8be679a20f07f885ea829
Wildan <wildanalifrio@gmail.com>
2025-02-21 08:08:48 +0700

    Initial Commit

---
+++ rock-paper-scissor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rock-paper-scissor
+//
+//  Created by Wildan on 21/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
